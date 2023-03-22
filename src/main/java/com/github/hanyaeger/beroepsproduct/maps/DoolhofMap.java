b4618eb8391ced1e8843570e5b861a5c5dfc61db
Oscar V <oscar.xbox15@gmail.com>
2023-03-22 15:22:37 +0100

    Temp fix Grid

--- src/main/java/com/github/hanyaeger/beroepsproduct/maps/DoolhofMap.java
+++ src/main/java/com/github/hanyaeger/beroepsproduct/maps/DoolhofMap.java
@@ -1,7 +1,7 @@
 package com.github.hanyaeger.beroepsproduct.maps;
 
 public class DoolhofMap {
-    public DoolhofMap(int id, Grid grid) {
+    public DoolhofMap(int id) {
 
     }
 
--- src/main/java/com/github/hanyaeger/beroepsproduct/maps/levelEenMap.java
+++ src/main/java/com/github/hanyaeger/beroepsproduct/maps/levelEenMap.java
@@ -3,6 +3,6 @@ package com.github.hanyaeger.beroepsproduct.maps;
 public class levelEenMap extends DoolhofMap {
     public Grid grid = new Grid();
     public levelEenMap() {
-        super(1, grid);
+        super(1);
     }
 }
