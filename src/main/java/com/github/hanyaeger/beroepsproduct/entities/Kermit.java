e247fd252fc915814b3c7b6ed9f75206560528e2
Umid1710 <umidalimi@hotmail.com>
2023-03-24 10:52:53 +0100

    Bordertouch

--- src/main/java/com/github/hanyaeger/beroepsproduct/entities/Kermit.java
+++ src/main/java/com/github/hanyaeger/beroepsproduct/entities/Kermit.java
@@ -4,17 +4,23 @@ import com.github.hanyaeger.api.Coordinate2D;
 import com.github.hanyaeger.api.Size;
 import com.github.hanyaeger.api.entities.Collided;
 import com.github.hanyaeger.api.entities.Collider;
+import com.github.hanyaeger.api.entities.SceneBorderTouchingWatcher;
 import com.github.hanyaeger.api.entities.impl.DynamicSpriteEntity;
+import com.github.hanyaeger.api.scenes.SceneBorder;
 
-public class Kermit extends DynamicSpriteEntity implements Collided {
+public class Kermit extends DynamicSpriteEntity implements Collided, SceneBorderTouchingWatcher {
 
     public Kermit(Coordinate2D location, Size size) {
         super("entities/Kermit.gif", location, size);
     }
 
+    public void notifyBoundaryTouching(final SceneBorder border) {
+        this.stopKermit();
+    }
     @Override
     public void onCollision(Collider collider) {
          this.stopKermit();
+
         // Zet stil, stop met bewegen.
     }
 
