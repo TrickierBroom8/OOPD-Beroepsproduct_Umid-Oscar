467b8c525d8d176995da091ba49af0bb4fbd73ac
Umid1710 <umidalimi@hotmail.com>
2023-03-27 11:17:07 +0200

    New maps

---
+++ src/main/java/com/github/hanyaeger/beroepsproduct/maps/LevelDrieMap.java
@@ -0,0 +1,54 @@
+package com.github.hanyaeger.beroepsproduct.maps;
+
+import com.github.hanyaeger.api.Coordinate2D;
+import com.github.hanyaeger.api.Size;
+import com.github.hanyaeger.api.entities.YaegerEntity;
+import com.github.hanyaeger.api.scenes.TileMap;
+import com.github.hanyaeger.beroepsproduct.entities.DeurEntity;
+import com.github.hanyaeger.beroepsproduct.entities.HegEntity;
+import com.github.hanyaeger.beroepsproduct.entities.Kermit;
+import com.github.hanyaeger.beroepsproduct.entities.PadEntityForTileMap;
+
+
+public class LevelDrieMap extends TileMap {
+
+    public LevelDrieMap(final Coordinate2D location, final Size size ) {
+        super(location, size);
+    }
+
+    @Override
+    public void setupEntities() {
+        addEntity(1, PadEntityForTileMap.class, PadEntityForTileMap.PadType.PAD);
+        addEntity(2, HegEntity.class);
+        addEntity(3, DeurEntity.class);
+    }
+
+    @Override
+    public int[][] defineMap() {
+        return new int[][]  {
+                {1, 1, 2, 2, 1, 1, 1, 1, 1, 2, 1, 1, 1, 2, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2},
+                {1, 1, 1, 1, 1, 2, 2, 2, 1, 1, 1, 2, 2, 2, 2, 1, 2, 1, 2, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1, 2},
+                {2, 1 ,2, 2, 1, 2, 3, 2, 1, 2, 1, 1, 1, 2, 2, 1, 2, 1, 1, 1, 2, 1 ,2, 2, 1, 2 ,2, 2, 1, 2 ,1, 2},
+                {1, 1, 1, 2, 1, 2, 1, 1, 1, 2, 1, 2, 1, 1, 3, 1, 2, 2, 2, 2, 2, 1, 2, 2, 1, 1, 1, 2, 1, 2, 1, 2},
+                {1, 2, 1, 2, 1, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 2, 1, 2},
+                {3, 2, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 1, 2 ,2, 2, 1, 2 ,2, 2, 1, 2 ,2, 1, 2, 2 ,2, 1, 2},
+                {1, 1, 1, 1, 1, 2, 2 ,1, 2, 1, 2, 1, 2, 1 ,1, 1, 2 ,1 ,2 ,1, 2 ,1 ,2, 1, 2 ,2 ,2 ,2, 1 ,1 ,1 ,2},
+                {2, 2, 2, 2, 1, 1, 1, 1, 2, 1, 2, 1, 1, 1, 2, 2, 2, 1, 1, 1, 2, 1, 1, 1, 1, 2, 2, 2, 1, 2, 2, 2},
+                {3, 2, 1, 1, 1, 2, 2, 2, 2, 1, 2, 2, 1, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 1, 2, 2, 1, 1, 2, 2, 2},
+                {1, 1, 1, 2, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 ,2, 2, 1, 1, 1, 1, 1, 1 ,1, 1},
+                {2, 2, 2, 2, 1, 2, 1, 2, 1, 2, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 1, 1, 1, 1, 2, 2, 2, 1, 2, 2, 1},
+                {1, 1, 1, 1, 1, 2, 1, 2, 1, 2, 2, 1, 2, 2, 2, 2, 1, 2, 2, 2, 1, 1, 2, 2, 1, 2, 2, 2, 1, 2, 1, 1},
+                {2, 2, 2, 2, 1, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 2, 1, 2, 2, 2, 1, 2, 2, 1, 1, 2, 2, 2},
+                {2, 1, 1, 2, 1, 2, 1, 2, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 1, 1, 1, 2, 1, 1, 1, 1, 2, 2, 1, 2, 1, 1},
+                {2, 1, 2, 2, 1, 2 ,1, 1, 1, 1, 2, 2, 1, 1, 1, 1, 2, 2, 1, 2, 2, 2, 1, 2, 2, 1 ,2, 2, 1, 2, 1, 2},
+                {2, 1, 1, 1, 1, 2, 2, 2, 2, 1, 2, 2, 1, 2, 2, 2, 2, 2, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 1, 2},
+                {2, 1, 2, 2, 2, 2, 2, 1, 2, 1, 1, 1, 1, 2, 2, 1, 1, 2, 2, 2, 1, 2, 2, 1, 2, 2, 1, 2, 2, 2, 1, 2},
+                {2, 1, 1, 2, 2, 2, 2, 1, 1, 1, 2, 2, 1, 2, 2, 2, 1 ,2, 1, 2, 1, 2, 1, 1, 2 ,2, 1, 1, 1, 1, 1, 2},
+                {2, 2, 2, 2, 2, 2 ,2 ,2, 2 ,2 ,2 ,2, 1, 1, 1, 1, 1, 2, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2},
+
+
+
+
+        };
+    }
+}
